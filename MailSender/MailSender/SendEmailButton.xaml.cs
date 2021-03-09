29ac0d94c8e1e61a7313b9c71d9162ffd33b54f7
bymagka <bymagka@bk.ru>
2021-03-09 19:37:31 +0300

    test1

--- MailSender/MailSender/SendEmailButton.xaml.cs
+++ MailSender/MailSender/SendEmailButton.xaml.cs
@@ -38,21 +38,18 @@ namespace MailSender
             string login = Application.Current.Resources["login"].ToString();
             string password = Application.Current.Resources["password"].ToString();
 
-            // отправитель - устанавливаем адрес и отображаемое в письме имя
             MailAddress from = new MailAddress(login, "Bymagka");
-            // кому отправляем
             MailAddress to = new MailAddress(login);
-            // создаем объект сообщения
+           
             MailMessage m = new MailMessage(from, to);
-            // тема письма
+            
             m.Subject = "Тест";
-            // текст письма
+            
             m.Body = "Тест";
-            // письмо представляет код html
-            m.IsBodyHtml = true;
-            // адрес smtp-сервера и порт, с которого будем отправлять письмо
+                   
+            
             SmtpClient smtp = new SmtpClient("smtp.mail.ru", 587);
-            // логин и пароль
+            
             smtp.Credentials = new NetworkCredential(login, password);
             smtp.EnableSsl = true;
             smtp.Send(m);
